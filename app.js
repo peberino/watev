1ba225d4a32fb0c48476b3a7a96c441ee93825a5
peberino <106129284+peberino@users.noreply.github.com>
2022-07-30 09:22:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,266 @@
+// const clueGame = {};
+// clueGame.confidentialCard = {};
+
+// clueGame.characters = [
+//   "Dwight Fairfield",
+//   "David King",
+//   "Claudette Morell",
+//   "Jake Park",
+//   "Meg Thomas",
+// ];
+// clueGame.weapons = [
+//   "Baseball Bat",
+//   "Lead Pipe",
+//   "Butcher Knife",
+//   "Handgun",
+//   "Screwdriver",
+// ];
+// clueGame.rooms = [
+//   "Observatory",
+//   "The Foyer",
+//   "Basement",
+//   "Attic",
+//   "Library",
+//   "Kitchen",
+// ];
+
+// clueGame.confidentialCard.murderer = clueGame.characters[0];
+// clueGame.confidentialCard.weapon = clueGame.weapons[2];
+// clueGame.confidentialCard.room = clueGame.rooms[3];
+
+// // Confidential card information
+// const { murderer, weapon, room } = clueGame.confidentialCard;
+
+// function createSuspectObjects(name) {
+//   return {
+//     name: name,
+//     color: name.split(" ")[1],
+//     speak() {
+//       console.log(`My name is ${name}`);
+//     },
+//   };
+// }
+
+// const suspects = ["Miss Scarlet", "Colonel Mustard", "Mr. White"];
+// const suspectsList = [];
+
+// // suspects.forEach((name) => {
+// //   suspectsList.push(createSuspectObjects(name));
+// // });
+
+// const mappedSuspects = suspects.map(createSuspectObjects);
+// console.log(mappedSuspects);
+
+// const videoData = [
+//   {
+//     name: "Miss Scarlet",
+//     present: true,
+//     rooms: [
+//       { kitchen: false },
+//       { ballroom: false },
+//       { conservatory: true },
+//       { "dining room": true },
+//       { "billiard room": false },
+//       { library: true },
+//     ],
+//   },
+//   {
+//     name: "Mrs. White",
+//     present: false,
+//     rooms: [
+//       { kitchen: false },
+//       { ballroom: false },
+//       { conservatory: false },
+//       { "dining room": false },
+//       { "billiard room": false },
+//       { library: false },
+//     ],
+//   },
+//   {
+//     name: "Reverend Green",
+//     present: true,
+//     rooms: [
+//       { kitchen: true },
+//       { ballroom: false },
+//       { conservatory: false },
+//       { "dining room": false },
+//       { "billiard room": true },
+//       { library: false },
+//     ],
+//   },
+//   {
+//     name: "Rusty",
+//     present: false,
+//     rooms: [
+//       { kitchen: false },
+//       { ballroom: false },
+//       { conservatory: false },
+//       { "dining room": false },
+//       { "billiard room": false },
+//       { library: false },
+//     ],
+//   },
+//   {
+//     name: "Colonel Mustard",
+//     present: true,
+//     rooms: [
+//       { kitchen: false },
+//       { ballroom: false },
+//       { conservatory: true },
+//       { "dining room": false },
+//       { "billiard room": true },
+//       { library: false },
+//     ],
+//   },
+//   {
+//     name: "Professor Plum",
+//     present: true,
+//     rooms: [
+//       { kitchen: false },
+//       { ballroom: false },
+//       { conservatory: false },
+//       { "dining room": false },
+//       { "billiard room": false },
+//       { library: false },
+//     ],
+//   },
+// ];
+
+// const presentPeople = videoData
+//   .filter((person) => {
+//     return person.present === true;
+//   })
+//   .map((person) => {
+//     return person.name;
+//   });
+
+// console.log(presentPeople);
+
+// const increment = (n) => {
+//   return n + 1;
+// };
+
+// const square = (n) => {
+//   return n * n;
+// };
+
+// const doMathSoIDontHaveTo = (n, func) => {
+//   return func(n);
+// };
+
+// console.log(doMathSoIDontHaveTo(5, square));
+
+const newDevelopment = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { "dining room": true },
+      { "billiard room": false },
+      { library: true },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": true },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { "dining room": false },
+      { "billiard room": true },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": true },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+
+let giveEmptyRooms = (array) => {
+  let emptyRooms = [];
+
+  for (let suspect of array) {
+    let emptyObj = {};
+    for (let room of suspect.rooms) {
+      if (Object.values(room)[0] === false) {
+        emptyObj[`${Object.keys(room)[0]}`] = false;
+      }
+    }
+    emptyRooms.push(emptyObj);
+  }
+
+  let [
+    totalKitchen,
+    totalBallroom,
+    totalConservatory,
+    totalBilliard,
+    totalDining,
+    totalLibrary,
+  ] = [0, 0, 0, 0, 0, 0];
+
+  for (let rooms of emptyRooms) {
+    for (let i = 0; i < Object.keys(rooms).length; i++) {
+      if (Object.keys(rooms)[i] === "kitchen") {
+        totalKitchen++;
+      } else if (Object.keys(rooms)[i] === "ballroom") {
+        totalBallroom++;
+      } else if (Object.keys(rooms)[i] === "billiard room") {
+        totalBilliard++;
+      } else if (Object.keys(rooms)[i] === "conservatory") {
+        totalConservatory++;
+      } else if (Object.keys(rooms)[i] === "library") {
+        totalLibrary++;
+      } else if (Object.keys(rooms)[i] === "dining room") {
+        totalDining++;
+      }
+    }
+  }
+
+  if (totalKitchen === 4) {
+    console.log("No one was present at the kitchen.");
+    return "No one was present at the kitchen.";
+  } else if (totalBallroom === 4) {
+    console.log("No one was present at the ballroom.");
+    return "No one was present at the ballroom.";
+  } else if (totalBilliard === 4) {
+    console.log("No one was present at the billiard room.");
+    return "No one was present at the billiard room.";
+  } else if (totalConservatory === 4) {
+    console.log("No one was present at the conservatory.");
+    return "No one was present at the conservatory.";
+  } else if (totalDining === 4) {
+    console.log("No one was present at the dining room.");
+    return "No one was present at the dining room.";
+  } else if (totalLibrary === 4) {
+    console.log("No one was present at the library.");
+    return "No one was present at the library.";
+  }
+};
+
+giveEmptyRooms(newDevelopment);
+// let emptyRooms = giveEmptyRooms(newDevelopment);
